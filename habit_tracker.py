749b9cc6c925b5b18f91714f932b9d1c1a317261
andreeaburcea <94985098+andreeaburcea@users.noreply.github.com>
2022-04-06 11:57:42 +0300

    Add files via upload

---
+++ habit_tracker.py
@@ -0,0 +1,74 @@
+import requests
+from datetime import datetime
+
+# Using https://pixe.la/ API for tracking habits
+
+USERNAME = "YOUR USERNAME"
+TOKEN = "YOUR SELF GENERATED TOKEN" # we have to generate our api key by ourselves for authentication
+GRAPH_ID = "YOUR GRAPH ID"
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+# Step 1: creating an account
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+## POST
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+# Step 2: making our graph
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": GRAPH_ID,
+    "name": "Coding Graph",
+    "unit": "minutes",
+    "type": "int",
+    "color": "ajisai"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+} # we need this headers for updating our track habits
+
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+# Step 3: putting data on our tracker
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+# will ask you how many km you run
+today = datetime.now()
+# print(today.strftime("%Y%m%d"))
+
+pixel_data = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": input("How many kilometers did you cycle today? "),
+}
+
+response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+print(response.text)
+
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+new_pixel_data = {
+    "quantity": "4.5"
+}
+
+## PUT
+# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
+# print(response.text)
+
+
+# Step 4: deleting data from our tracker
+
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+## DELETE
+# response = requests.delete(url=delete_endpoint, headers=headers)
+# print(response.text)
